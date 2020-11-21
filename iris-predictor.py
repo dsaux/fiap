9fdeef090072ed81001dba934d3de3001a867a68
dsaux <73562833+dsaux@users.noreply.github.com>
2020-11-21 01:07:57 -0300

    Add files via upload

---
+++ iris-predictor.py
@@ -0,0 +1,13 @@
+import pickle
+
+pkl_filename = 'pickle_model.pkl'
+with open(pkl_filename, 'rb') as file:
+	pickle_model = pickle.load(file)
+
+vLista = []
+while True:
+	input_str = input("Informe os 4 valores separados por virgula: ")
+	str_List = input_str.split(',')
+	values = [float(item) for item in str_List]
+	res = pickle_model.predict([values])
+	print(res)
