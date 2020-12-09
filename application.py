6efab2d5226ba66075645988ef4ddd5b0e4c8fbd
dsaux <73562833+dsaux@users.noreply.github.com>
2020-12-09 15:29:06 -0300

    Update application.py

--- application.py
+++ application.py
@@ -6,16 +6,16 @@ import test
 @app.route("/")
 def hello():
     return "Hola Python!"
-#import pickle
+import pickle
 
-#pkl_filename = 'pickle_model.pkl'
-#with open(pkl_filename, 'rb') as file:
-# pickle_model = pickle.load(file)
+pkl_filename = 'pickle_model.pkl'
+with open(pkl_filename, 'rb') as file:
+ pickle_model = pickle.load(file)
 
-#vLista = []
-#while True:
-# input_str = input("Informe os 4 valores separados por virgula: ")
-# str_List = input_str.split(',')
-# values = [float(item) for item in str_list]
-# res = pickle_model.predict([values])
-# print(res)
+vLista = []
+while True:
+ input_str = input("Informe os 4 valores separados por virgula: ")
+ str_List = input_str.split(',')
+ values = [float(item) for item in str_list]
+ res = pickle_model.predict([values])
+ print(res)
