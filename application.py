9d1655a438525a83f414921954d76c76e8b5df20
dsaux <73562833+dsaux@users.noreply.github.com>
2020-12-09 17:10:28 -0300

    Update application.py

--- application.py
+++ application.py
@@ -1,13 +1,16 @@
-from flask import Flask, pickle
-#import pickle
+from flask import Flask, render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
+
 @app.route('/')
+def index():
+    return render_template('index.html')
 
-pkl_filename = 'pickle_model.pkl'
-with open(pkl_filename, 'rb') as file:
-    pickle_model = pickle.load(file)
+#import pickle
+#pkl_filename = 'pickle_model.pkl'
+#with open(pkl_filename, 'rb') as file:
+#    pickle_model = pickle.load(file)
 
-res = pickle_model.predict([[5.7, 3.2, 1.1, 3.6]])
-res
+#res = pickle_model.predict([[5.7, 3.2, 1.1, 3.6]])
+#res
 
